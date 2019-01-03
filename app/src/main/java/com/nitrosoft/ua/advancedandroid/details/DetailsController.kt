7dd2a0d1ee14db30477b002cf606bc2c47c410cf
Dmitrij Sorokopud <dmitrij.sorokopud@gmail.com>
2019-01-03 09:24:19 +0200

    *Details screen view added

---
+++ app/src/main/java/com/nitrosoft/ua/advancedandroid/details/DetailsController.kt
@@ -0,0 +1,10 @@
+package com.nitrosoft.ua.advancedandroid.details
+
+import com.nitrosoft.ua.advancedandroid.R
+import com.nitrosoft.ua.advancedandroid.base.BaseController
+
+class DetailsController: BaseController() {
+    override fun layoutRes(): Int {
+        return R.layout.scree_repo_details
+    }
+}
