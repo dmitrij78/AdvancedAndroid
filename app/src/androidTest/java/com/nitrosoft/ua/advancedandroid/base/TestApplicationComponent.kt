b32ac6015ee9a3510102946277965bfd017faf50
Dmitrij Sorokopud <dmitrij.sorokopud@gmail.com>
2019-01-30 09:32:06 +0200

    Перенос регистрации модуля  NavigationModule::class в Subcomponent TestMainActivityComponent

--- app/src/androidTest/java/com/nitrosoft/ua/advancedandroid/base/TestApplicationComponent.kt
+++ app/src/androidTest/java/com/nitrosoft/ua/advancedandroid/base/TestApplicationComponent.kt
@@ -3,7 +3,6 @@ package com.nitrosoft.ua.advancedandroid.base
 import com.nitrosoft.ua.advancedandroid.data.TestRepoServiceModule
 import com.nitrosoft.ua.advancedandroid.networking.ServiceModule
 import com.nitrosoft.ua.advancedandroid.trending.TrendingReposControllerTest
-import com.nitrosoft.ua.advancedandroid.ui.NavigationModule
 import dagger.Component
 import javax.inject.Singleton
 
@@ -12,8 +11,7 @@ import javax.inject.Singleton
     ApplicationModule::class,
     TestActivityBindingModule::class,
     TestRepoServiceModule::class,
-    ServiceModule::class,
-    NavigationModule::class
+	ServiceModule::class
 ])
 interface TestApplicationComponent : ApplicationComponent {
 
--- app/src/androidTest/java/com/nitrosoft/ua/advancedandroid/home/TestMainActivityComponent.kt
+++ app/src/androidTest/java/com/nitrosoft/ua/advancedandroid/home/TestMainActivityComponent.kt
@@ -1,14 +1,17 @@
 package com.nitrosoft.ua.advancedandroid.home
 
 import com.nitrosoft.ua.advancedandroid.di.ActivityScope
+import com.nitrosoft.ua.advancedandroid.ui.NavigationModule
 import dagger.Subcomponent
 import dagger.android.AndroidInjector
 
 @ActivityScope
 @Subcomponent(modules = [
-    TestScreenBindingModule::class
+	TestScreenBindingModule::class,
+	NavigationModule::class
 ])
 interface TestMainActivityComponent : AndroidInjector<MainActivity> {
-    @Subcomponent.Builder
+
+	@Subcomponent.Builder
     abstract class Builder : AndroidInjector.Builder<MainActivity>()
 }
