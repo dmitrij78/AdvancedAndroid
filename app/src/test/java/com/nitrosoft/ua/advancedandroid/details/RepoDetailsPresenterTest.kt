b0df1b6b467a1ff87258bf2f8071114c3a79ead2
Dmitrij Sorokopud <dmitrij.sorokopud@privatbank.ua>
2019-07-23 10:35:30 +0300

    RepoDetails unit tests

--- app/src/test/java/com/nitrosoft/ua/advancedandroid/details/RepoDetailsPresenterTest.kt
+++ app/src/test/java/com/nitrosoft/ua/advancedandroid/details/RepoDetailsPresenterTest.kt
@@ -3,11 +3,15 @@ package com.nitrosoft.ua.advancedandroid.details
 import com.nitrosoft.ua.advancedandroid.data.RepoRepository
 import com.nitrosoft.ua.advancedandroid.lifecycle.DisposableManager
 import com.nitrosoft.ua.advancedandroid.models.Contributor
+import com.nitrosoft.ua.advancedandroid.models.ContributorListItem
 import com.nitrosoft.ua.advancedandroid.models.Repo
 import com.nitrosoft.ua.advancedandroid.testutils.TestUtils
+import com.nitrosoft.ua.poweradapter.adapter.RecyclerDataSource
 import com.squareup.moshi.Types
 import io.reactivex.Single
+import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
@@ -20,9 +24,10 @@ class RepoDetailsPresenterTest {
     @Mock private lateinit var repoRepository: RepoRepository
     @Mock private lateinit var viewModel: RepoDetailsViewModel
     @Mock private lateinit var repoConsumer: Consumer<Repo>
-    @Mock private lateinit var contributorsConsumer: Consumer<List<Contributor>>
+    @Mock private lateinit var contributorsConsumer: Consumer<Any>
     @Mock private lateinit var detailsErrorConsumer: Consumer<Throwable>
     @Mock private lateinit var contributorsErrorConsumer: Consumer<Throwable>
+    @Mock private lateinit var recyclerDataSource: RecyclerDataSource
 
     private val repo: Repo = TestUtils.loadJson("mock/repos/get_repo.json", Repo::class.java)
     private val contributors: List<Contributor> = TestUtils.loadJson(
@@ -36,12 +41,16 @@ class RepoDetailsPresenterTest {
         const val NAME = "name"
     }
 
+    init {
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { Schedulers.trampoline() }
+    }
+
     @Before
     fun setUp() {
         MockitoAnnotations.initMocks(this)
 
         Mockito.`when`(viewModel.processRepo()).thenReturn(repoConsumer)
-        Mockito.`when`(viewModel.processContributors()).thenReturn(contributorsConsumer)
+        Mockito.`when`(viewModel.contributorsLoaded()).thenReturn(contributorsConsumer)
         Mockito.`when`(viewModel.detailsError()).thenReturn(detailsErrorConsumer)
         Mockito.`when`(viewModel.contributorsError()).thenReturn(contributorsErrorConsumer)
 
@@ -69,7 +78,7 @@ class RepoDetailsPresenterTest {
     fun repoContributors() {
         initPresenter()
 
-        Mockito.verify(contributorsConsumer).accept(contributors)
+        Mockito.verify(recyclerDataSource).setData(contributors.map { c -> ContributorListItem(c) })
     }
 
     @Test
@@ -83,6 +92,10 @@ class RepoDetailsPresenterTest {
     }
 
     private fun initPresenter() {
-        RepoDetailsPresenter(OWNER, NAME, Mockito.mock(DisposableManager::class.java), repoRepository, viewModel)
+        RepoDetailsPresenter(
+                OWNER,
+                NAME,
+                Mockito.mock(DisposableManager::class.java), repoRepository, viewModel, recyclerDataSource
+        )
     }
 }
