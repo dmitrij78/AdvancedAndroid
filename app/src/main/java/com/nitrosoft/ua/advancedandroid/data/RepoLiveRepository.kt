4f687bac2ad064c78101726993d0730656578c69
Dmitrij Sorokopud <dmitrij.sorokopud@privatbank.ua>
2019-09-30 17:06:01 +0300

    *RepoLiveRepository

---
+++ app/src/main/java/com/nitrosoft/ua/advancedandroid/data/RepoLiveRepository.kt
@@ -0,0 +1,39 @@
+package com.nitrosoft.ua.advancedandroid.data
+
+import androidx.lifecycle.LiveData
+import com.nitrosoft.ua.advancedandroid.models.Repo
+import io.reactivex.Scheduler
+import javax.inject.Inject
+import javax.inject.Named
+import javax.inject.Provider
+import javax.inject.Singleton
+
+@Singleton
+class RepoLiveRepository @Inject constructor(
+        private val repoRequesterProvider: Provider<RepoRequester>,
+        @Named("network_scheduler") private val scheduler: Scheduler,
+        private val rateLimiter: RateLimiter<String>
+
+) {
+
+    fun getTrendingRepos(): LiveData<List<Repo>> {
+        return object : NetworkBoundResource<List<Repo>, TrendingReposResponse>() {
+            override fun saveCallResult(item: TrendingReposResponse) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun shouldFetch(data: List<Repo>?): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun loadFromDb(): LiveData<List<Repo>> {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun createCall(): LiveData<TrendingReposResponse> {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        }.asLiveData()
+    }
+
+}
