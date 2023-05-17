c5cd4352db66e53efa8fa670c99ec5ff215dcaa1
Dmitrij Sorokopud <dmitrij.sorokopud@gmail.com>
2023-05-17 15:45:29 +0300

    [refactor] Update app dependencies. Version numbers as params

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,8 @@ android {
     }
 }
 
+val powerAdapterProject = ":poweradapter"
+
 val daggerVersion = "2.45"
 val dagger = "com.google.dagger:dagger:$daggerVersion"
 val daggerCompiler = "com.google.dagger:dagger-compiler:$daggerVersion"
@@ -93,7 +95,7 @@ val rxBinding = "com.jakewharton.rxbinding2:rxbinding-kotlin:$rxBindingVersion"
 val threeTenVersion = "1.2.1"
 val threeTen = "com.jakewharton.threetenabp:threetenabp:$threeTenVersion"
 
-val timberVersion = "1.2.1"
+val timberVersion = "5.0.1"
 val timber = "com.jakewharton.timber:timber:$timberVersion"
 
 val glideVersion = "4.9.0"
@@ -120,7 +122,7 @@ val espressoCore = "androidx.test.espresso:espresso-core:$espressoCoreVersion"
 
 dependencies {
 
-    implementation(project(":poweradapter"))
+    implementation(project(powerAdapterProject))
 
     implementation(androidXCore)
     implementation(cardView)
@@ -170,95 +172,6 @@ dependencies {
 
     androidTestImplementation(espressoCore)
 }
-
-
-//plugins {
-//    id("com.android.application")
-//    id("org.jetbrains.kotlin.android")
-//    id("kotlin-kapt")
-//}
-//
-//android {
-//    compileSdkVersion(33)
-//    defaultConfig {
-//        applicationId = rootProject.ext.applicationId
-//        minSdkVersion(rootProject.ext.minSdkVersion)
-//        targetSdkVersion(rootProject.ext.targetSdkVersion)
-//        versionCode = rootProject.ext.versionCode
-//        versionName = rootProject.ext.versionName
-//        testInstrumentationRunner = "com.nitrosoft.ua.advancedandroid.test.CustomTestRunner"
-//    }
-//    buildTypes {
-//        getByName("release") {
-//            isMinifyEnabled = true
-//            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
-//        }
-//        getByName("debug") {
-//            applicationIdSuffix = ".debug"
-//            isDebuggable = true
-//        }
-//    }
-//    compileOptions {
-//        sourceCompatibility = JavaVersion.VERSION_1_8
-//        targetCompatibility = JavaVersion.VERSION_1_8
-//    }
-//}
-//
-////kapt {
-////    arguments {
-////        // When using Java 9 and above
-////        arg("kotshi.generatedAnnotation", "javax.annotation.processing.Generated")
-////    }
-////}
-//
-//dependencies {
-//    implementationproject(":poweradapter")
-//
-//    implementation("androidx.core:core:$androidxVersion")
-//    implementation("androidx.cardview:cardview:$cardViewVersion")
-//    implementation("androidx.recyclerview:recyclerview:$recylerViewVersion")
-//    implementation("com.google.android.material:material:$materialVersion")
-//
-//    implementation("androidx.room:room-runtime:$archComponentVersion")
-//    kapt("androidx.room:room-compiler:$archComponentVersion")
-//
-//    implementation("androidx.room:room-rxjava2:$archComponentVersion")
-//
-//    implementation("com.google.dagger:dagger:$daggerVersion")
-//    implementation("com.google.dagger:dagger-android-support:$daggerVersion")
-//    kapt("com.google.dagger:dagger-compiler:$daggerVersion")
-//
-//    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
-//    implementation("com.squareup.retrofit2:converter-moshi:$retrofitVersion")
-//    implementation("com.squareup.retrofit2:adapter-rxjava2:$retrofitVersion")
-//    implementation("com.squareup.okhttp3:logging-interceptor:$okhttpVersion")
-//
-//    implementation("se.ansman.kotshi:api:$kotshiVersion")
-//    kapt("se.ansman.kotshi:compiler:$kotshiVersion")
-//
-//    implementation("com.jakewharton.threetenabp:threetenabp:$treeTenAppVersion")
-//
-//    implementation("io.reactivex.rxjava2:rxjava:$rxJavaVersion")
-//    implementation("io.reactivex.rxjava2:rxandroid:$rxAndroidVersion")
-//    implementation("com.jakewharton.rxrelay2:rxrelay:$rxRelayVersion")
-//    implementation("com.jakewharton.rxbinding2:rxbinding-kotlin:$rxBindingsVersion")
-//
-//    implementation("com.jakewharton.timber:timber:$timberVersion")
-//
-//    implementation("com.github.bumptech.glide:glide:$glideVersion")
-//    kapt("com.github.bumptech.glide:compiler:$glideVersion")
-//
-//    testimplementation("junit:junit:$junitVersion")
-//    testimplementation("org.mockito:mockito-core:$mockitoVersion")
-//
-//    kaptAndroidTest ("com.google.dagger:dagger-compiler:$daggerVersion")
-//
-//    androidTestimplementation("androidx.test:core:$testCoreVersion")
-//
-//    androidTestimplementation("androidx.test:runner:$testRunnerVersion")
-//    androidTestimplementation("androidx.test:rules:$testRulesVersion")
-//    androidTestimplementation("androidx.test.espresso:espresso-core:$espressoVersion")
-//}
 
 //def mockCopySpec = copySpec {
 //    from rootProject . file ('app/')
